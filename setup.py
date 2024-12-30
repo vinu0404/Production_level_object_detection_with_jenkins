443c6d626ac9a2b2094d9902228a682050d20338
vinu0404 <22cd3034@rgipt.ac.in>
2024-12-30 16:35:36 +0530

    data collection

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'signLanguages',
+    version= '0.0.0',
+    author= 'vinu',
+    author_email= '22cd3034@rgipt.ac.in',
+    packages= find_packages(),
+    install_requires = []
+
+)
